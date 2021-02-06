b3e8069883d3fb29186ec769406d84b83c93e57a
Alan Sampson <alansamps@gmail.com>
2021-02-06 04:44:14 +0000

    Remove dead code.

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ struct student {
 };
 
 void student_grades(std::vector<student> & students);
-void display_original(std::vector<student> const & students);
+void display_table(std::vector<student> const & students);
 void order_by_name(std::vector<student> & students);
 void order_by_gender(std::vector<student> & students);
 void rank_high(std::vector<student> & students);
@@ -74,38 +74,9 @@ int main() {
 
   student_grades(students);
 
-#ifdef HISWAY_
-  std::string StudentN[41] = {
-    "", "Jake Jeric Tan", "Rico James Abella ",
-    "Trishia Valerie Acas ", "Jenny Mae Adventurado ",
-    "Shan Forrest Almia ", "Maebelle Marie Aniscal ",
-    "Amanda Jan Alexis Babao ", "Hans Marius Baranquil ",
-    "Zairrah Shynne Bayawa ", "Jonathan Carl Catipay ",
-    "Aeisha Jewel Ceniza", "Carmelle Centino",
-    "Carmelo Anthony Clar", "Arabella Constantino",
-    "Bianca Andreah Cubar", "Magen Delgado", "Bernard Dicdican",
-    "Mary Claire Enriquez", "Maxene Espina",
-    "Kierth Jerland Figues", "Althea Aubrey Herrera",
-    "Justine Kylie Itol", "Kheilah Marie Ligtas",
-    "Marea Noelle Magnanao", "Alyssa Kaithlyn Makiling",
-    "Karlos Daniel Masecampo", "Jieuel Migabon",
-    "James Edward Nepangue", "Jule Daniel Ocariza",
-    "Fiona Elaiza Dela Parazo", "Ethan Daryl Quijada",
-    "Cris Paolo Rafols", "Aiah Michelley Rizarri",
-    "Lear Lorenz Roma", "Kirsten Saavedra", "Clare Marie Salazar","Michael Louie Secapuri", "Maria Sofia Sevilla",
-    "Kammerdeiner Solon", "Chielsea Tahanlangit",
-  };
-  double writtenw[41]      = { 75., 80., 82., 90., 80., };
-  double peformancetsk[41] = { 85., 83., 84., 70., 72., };
-  double Quartassys[41]    = { 85., 80., 87., 89., 75., };
-  double Finalgrades[41];
-#endif /* HISWAY_ */
-
   bool looping(true);
   while (looping) {
-//Yesno:
     char choice;
-    std::string remarks, Yesno;
     std::cout << "   Menu of Choices\n"
               << "a. Original Display\n"
               << "b. Names in alphabetical Order\n"
@@ -121,198 +92,31 @@ int main() {
     switch (choice) {
     case 'a':
     case 'A':
-      display_original(students);
-  
-#ifdef HISWAY_
-    std::cout << std::setw(40) << "COMPUTER PROJECT\n"
-              << std::setw(43) << "ACADEMIC YEAR 2020- 2021\n\n"
-              << std::setw(43) << "UNIVERSITY OF SAN CARLOS\n"
-              << std::setw(51) << "BASIC EDUCATION DEPARTMENT- SOUTH CAMPUS\n"
-              << std::setw(44) << "J. Alcantara St., Cebu City\n\n"
-              << "Section: Grade 10 BMHS"
-              << std::setw(29) << "Subject:TLE\n"
-              << "Adviser: Daegie Montermoso"
-              << std::setw(45) << "Teacher: Engr. Dang B. Guanzon\n\n"
-              << "Student Names"
-              << std::setw(16) << "WW"
-              << std::setw(4) << "PT"
-              << std::setw(4) << "QA"
-              << std::setw(14) << "FINAL GRADE"
-              << std::setw(10) << "REMARKS"
-              << std::endl;
-    for (int z = 1; z <= 40; z++) {
-      Finalgrades[z] = .20 * writtenw[z] + .60 * peformancetsk[z] + 20 * Quartassys[z];
-      if (Finalgrades[z] >= 75) {
-        remarks = "Passed";
-      }
-      else {
-        remarks = "Fail";
-      }
-
-      std::cout << std::left << z << "." << std::setw(30) << StudentN[z] << std::setw(4) << writtenw[z] <<
-      std::setw(4) << peformancetsk[z] << std::setw(4) << Quartassys[z] << std::setw(50)
-      << Finalgrades[z] << std::endl;
-    }
-    std::cout << "\n"
-              << "Submitted by: "
-              << std::setw(50)
-              << "\nArithmetic Mean"
-              << std::setw(4)
-              << "\nChecked by: "
-              << std::setw(50)
-              << "\n% Passing\n"
-              << std::setw(20) << '\n'
-              << "Jake Jeric T. Tan\n"
-              << std::setw(40)
-              << "\n% Failling\n"
-              << std::setw(20)
-              << "Engr. Dang B. Guanzon\n"
-              << "Student Name\n"
-              << std::setw(50)
-              << "Computer Teacher"
-              << std::endl;
-
-  //system("cls");
-
-        // cout<<"Go back [y/n]? : ";
-        // cin>>Yesno;
-        // if (Yesno=="y" || Yesno=="Y")
-        // goto Yesno;
-        //  else
-        //  cout<<"Thank you for your time..."<<endl;
-#endif /* HISWAY_ */
+      display_table(students);
       break;
 
     case 'b':
     case 'B':
       order_by_name(students);
-      display_original(students);
-
-#ifdef HISWAY_
-    std::cout << std::setw(40) << "COMPUTER PROJECT\n"
-              << std::setw(43) << "ACADEMIC YEAR 2020- 2021\n"
-              << "\n"
-              << std::setw(43) << "UNIVERSITY OF SAN CARLOS\n"
-              << std::setw(51) << "BASIC EDUCATION DEPARTMENT- SOUTH CAMPUS\n"
-              << std::setw(44)<<"J. Alcantara St., Cebu City\n"
-              << "\n"
-              << "Section: Grade 10 BMHS"
-              << std::setw(29) << "Subject:TLE\n"
-              << "Adviser: Daegie Montermoso"
-              << std::setw(45) << "Teacher: Eleuteria Dang Guanzon\n"
-              << "\n"
-              << "Student Names"
-              << std::setw(16) << "WW"
-              << std::setw(4) << "PT"
-              << std::setw(4) << "QA"
-              << std::setw(14) << "FINAL GRADE"
-              << std::setw(10) << "REMARKS"
-              << std::endl;
-    for (int z = 1; z <= 40; z++) {
-      Finalgrades[z] = .20 * writtenw[z] +
-                       .60 * peformancetsk[z] +
-                        20 * Quartassys[z];
-      std::cout << std::left << z << "." 
-                << std::setw(25) << StudentN[z]
-                << std::setw(4)<< std::fixed
-                << std::setprecision(0)
-                << writtenw[z]
-                << std::setw(4) << std::fixed
-                << std::setprecision(0)
-                << peformancetsk[z]
-                << std::setw(4) << std::fixed
-                << std::setprecision(0)
-                << Quartassys[z]
-                << std::setw(4) << Finalgrades[z]
-                << std::setw(4) << std::fixed
-                << std::setprecision(0)
-                << std::endl;
-    }
-#endif /* HISWAY_ */
+      display_table(students);
       break;
 
     case 'c':
     case 'C':
       order_by_gender(students);
-      display_original(students);
-
-#ifdef HISWAY_
-    std::cout << std::setw(40) << "COMPUTER PROJECT\n"
-              << std::setw(43) << "ACADEMIC YEAR 2020- 2021\n"
-              << "\n"    
-              << std::setw(43) << "UNIVERSITY OF SAN CARLOS\n"
-              << std::setw(51) << "BASIC EDUCATION DEPARTMENT- SOUTH CAMPUS\n"
-              << std::setw(44) << "J. Alcantara St., Cebu City\n"
-              << "\n"
-              << "Section: Grade 10 BMHS"
-              << std::setw(29) << "Subject:TLE\n"
-              << "Adviser: Daegie Montermoso"
-              << std::setw(45) << "Teacher: Eleuteria Dang Guanzon\n"
-              << "\n"
-              << "Student Names"
-              << std::setw(16) << "WW"
-              << std::setw(4) << "PT"
-              << std::setw(4) << "QA"
-              << std::setw(14) << "FINAL GRADE"
-              << std::setw(10) << "REMARKS"
-              << std::endl;
-#endif /* HISWAY_ */
+      display_table(students);
       break;
 
     case 'd':
     case 'D':
       rank_high(students);
-      display_original(students);
-
-#ifdef HISWAY_
-    std::cout << std::setw(40) << "COMPUTER PROJECT\n"
-              << std::setw(43) << "ACADEMIC YEAR 2020- 2021\n"
-              << "\n"
-              << std::setw(43) << "UNIVERSITY OF SAN CARLOS\n"
-              << std::setw(51) << "BASIC EDUCATION DEPARTMENT- SOUTH CAMPUS\n"
-              << std::setw(44) << "J. Alcantara St., Cebu City\n"
-              << "\n"
-              << "Section: Grade 10 BMHS"
-              << std::setw(29) << "Subject:TLE\n"
-              << "Adviser: Daegie Montermoso"
-              << std::setw(45) << "Teacher: Eleuteria Dang Guanzon\n"
-              << "\n"
-              << "Student Names"
-              << std::setw(16) << "WW"
-              << std::setw( 4) << "PT"
-              << std::setw( 4) << "QA"
-              << std::setw(14) << "FINAL GRADE"
-              << std::setw(10) << "REMARKS"
-              << std::endl;
-#endif /* HISWAY_ */
+      display_table(students);
       break;
 
     case 'e':
     case 'E':
       rank_low(students);
-      display_original(students);
-
-#ifdef HISWAY_
-    std::cout << std::setw(40) << "COMPUTER PROJECT\n"
-              << std::setw(43) << "ACADEMIC YEAR 2020- 2021\n"
-              << "\n"
-              << std::setw(43) << "UNIVERSITY OF SAN CARLOS\n"
-              << std::setw(51) << "BASIC EDUCATION DEPARTMENT- SOUTH CAMPUS\n"
-              << std::setw(44) << "J. Alcantara St., Cebu City\n"
-              << "\n"
-              << "Section: Grade 10 BMHS"
-              << std::setw(29) << "Subject:TLE\n"
-              << "Adviser: Daegie Montermoso"
-              << std::setw(45) << "Teacher: Eleuteria Dang Guanzon\n"
-              << "\n"
-              << "Student Names"
-              << std::setw(16) << "WW"
-              << std::setw(4) << "PT"
-              << std::setw(4) << "QA"
-              << std::setw(14) << "FINAL GRADE"
-              << std::setw(10) << "REMARKS"
-              << std::endl;
-#endif /* HISWAY_ */
+      display_table(students);
       break;
 
     case 'f':
@@ -348,7 +152,7 @@ void student_grades(std::vector<student> & students) {
   }
 }
 
-void display_original(std::vector<student> const & students) {
+void display_table(std::vector<student> const & students) {
   std::cout << std::setw(40) << "COMPUTER PROJECT\n"
             << std::setw(43) << "ACADEMIC YEAR 2020- 2021\n\n"
             << std::setw(43) << "UNIVERSITY OF SAN CARLOS\n"
